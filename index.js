903e083fe0d6f58783ede6e275257c1fb83dc7e1
Kritizen <kritizengames@gmail.com>
2023-10-15 12:14:30 -0500

    more changes

--- index.js
+++ index.js
@@ -5,13 +5,27 @@ const app = express();
 const port = 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+const tasks = [];
+const workTasks = [];
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", {tasks: tasks});
 });
 
 app.get("/work", (req, res) => {
-    res.render("work.ejs");
+    res.render("work.ejs", {workTasks: workTasks});
+});
+
+app.post("/", (req, res) => {
+  const task = req.body.task;
+  tasks.push(task);
+  res.redirect("/");
+});
+
+app.post("/work", (req, res) => {
+  const workTask = req.body.task;
+  workTasks.push(workTask);
+  res.redirect("/work");
 });
 
 app.listen(port, () => {
